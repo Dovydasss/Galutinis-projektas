6cc726451fa6e0339e1e16ff8d0e00c15028c6d1
Dovydas Stanelis <dovydas.stanelis1@gmail.com>
2022-06-04 21:09:38 +0300

    test

--- src/main/java/lt/viko/eif/api/Galutinisprojektas/controller/MissionController.java
+++ src/main/java/lt/viko/eif/api/Galutinisprojektas/controller/MissionController.java
@@ -1,7 +1,22 @@
 package lt.viko.eif.api.Galutinisprojektas.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lt.viko.eif.api.Galutinisprojektas.model.Mission;
+import lt.viko.eif.api.Galutinisprojektas.model.Team;
+import lt.viko.eif.api.Galutinisprojektas.service.MissionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mission")
 public class MissionController {
+
+    MissionService missionService;
+
+    public MissionController(MissionService missionService){this.missionService = missionService;}
+
+    @PostMapping
+    public ResponseEntity<Mission> saveMission(@RequestBody Mission mission){
+        return new ResponseEntity<Mission>(missionService.saveMission(mission), HttpStatus.CREATED);
+    }
 }
